39a337c45c065afb047c2c2b36f6731b749c06a3
Alexis Sushko <alexis.sushko@gmail.com>
2024-10-17 12:57:55 -0400

    Seeded the database with 1 user and 3 tasks

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,19 @@
-const prisma = require("../prisma");
-const seed = async () => {};
+const prisma = require("./index");
+const { faker } = require("@faker-js/faker");
+const seed = async (numUser = 1, numTasks = 3) => {
+  const tasks = Array.from({ length: numTasks }, () => ({
+    name: faker.internet.displayName(),
+  }));
+
+  await prisma.user.create({
+    data: {
+      username: "fakeuser",
+      password: "password",
+      tasks: { create: tasks },
+    },
+  });
+  //await prisma.task.createMany({ data: tasks, owner: { connect: user } });
+};
 seed()
   .then(async () => await prisma.$disconnect())
   .catch(async (e) => {
